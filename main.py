0b4fb0b09009762c389309d80126cc6ea87ad5e3
Vinicius Borin <borin.vini@gmail.com>
2024-06-25 12:12:21 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close Window
+            quit()  # end pygame
