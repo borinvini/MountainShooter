379010daaec9e6e30e75c49f702adb1d60cacd2d
Vinicius Borin <borin.vini@gmail.com>
2024-06-28 09:31:24 -0300

    Adding classes generated from StarUML

---
+++ code/Level.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
+
---
+++ code/Player.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player(Entity):
+    def __init__(self):
+        pass
+
+    def update(self, ):
+        pass
+
+    def move(self, ):
+        pass
+
