a69d89705e28170f395204ab29399ea6c3d89e38
Vinicius Borin <borin.vini@gmail.com>
2024-07-04 13:38:15 -0300

    Fixing imports in Level.py file

--- code/Level.py
+++ code/Level.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
-import pygame
+# -*- coding: utf-8 -*-
+import sys
 
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import COLOR_WHITE, WIN_HEIGHT
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -13,10 +18,11 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.timeout = 20000  # 20 segundos
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
-        pygame.mixer_music.play(-1))
+        pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
@@ -35,11 +41,8 @@ class Level:
             pygame.display.flip()
         pass
 
-
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
-
-
